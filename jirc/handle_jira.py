f801465930bffc6f59b9394eb5247628a3a1ac4f
pavol kutaj <pkutaj@gmail.com>
2022-02-05 08:58:42 +0000

    <fix> webbrowser opening

--- jirc/handle_jira.py
+++ jirc/handle_jira.py
@@ -9,19 +9,23 @@ jira_server = {'server': config["jira_server"]}
 jira = JIRA(options=jira_server, basic_auth=(
     config["jira_user"], config["jira_token"]))
 
+
 def get_jira_projects():
     print_JIRA_project(config["jira_project"])
     return jira.project(config["jira_project"])
 
+
 def create_card(issue_type):
     issue_dict = {
-        'project': {'key': 'SPT'},
-        'summary': input('Card Title: '),
-        'issuetype': {'name': issue_type}
+        "project": config["jira_project"],
+        "summary": input('Card Title: '),
+        "issuetype": issue_type
     }
-    pp(issue_dict)
     new_issue = jira.create_issue(fields=issue_dict)
     jira_card_url = new_issue.permalink()
-    if(input("Browser? (y/N):")):
+    del(issue_dict["project"], issue_dict["issuetype"])
+    issue_dict["url"] = jira_card_url
+    issue_dict["key"] = new_issue.key
+    pp(issue_dict)
+    if(input("Browser? (y/N):")) == "y" or "Y":
         webbrowser.open(jira_card_url)
-    print(f"MrD created {new_issue.key} at {jira_card_url}")
