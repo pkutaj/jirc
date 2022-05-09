d8f4f5165b57552b53db8055a8308704a961c9a4
pavol kutaj <pkutaj@gmail.com>
2022-05-09 11:31:49 +0000

    <refactor> reformat with black

--- __main__.py
+++ __main__.py
@@ -7,12 +7,11 @@ def main():
     for i, issue in enumerate(jira_project.issueTypes):
         print(i, issue.name)
     issue_or_project = input("Enter number OR 'c' for project change: ")
-    if issue_or_project == ('c' or 'C'):
+    if issue_or_project == ("c" or "C"):
         change_jira_project()
         main()
     else:
-        create_card(
-            issue_id=jira_project.issueTypes[int(issue_or_project)].id)
+        create_card(issue_id=jira_project.issueTypes[int(issue_or_project)].id)
 
 
 if __name__ == "__main__":
